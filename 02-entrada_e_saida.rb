02dcc679ace668c730100b165548de7fa690d1b4
Rafael Oliveira <rafa.forsoft.25@gmail.com>
2018-01-02 21:05:34 -0200

    iniciação do curso do udemy

---
+++ 02-entrada_e_saida.rb
@@ -0,0 +1,10 @@
+puts "olá"
+puts "Digite o seu nome: "
+v1 = gets.chomp
+
+puts "O seu nome é: " + v1
+
+puts "Digite a sua idade: "
+v2 = gets.chomp
+
+puts "Sua idade é: " + v2
---
+++ 03-strings.rb
@@ -0,0 +1,7 @@
+v1 = 'Rafael'
+puts "Hello, " + v1
+
+v2 = 'Rafael Oliveira'
+puts "Beleza? #{v2}"
+
+puts "a soma de 1 + 2 é: #{1 + 2}"
